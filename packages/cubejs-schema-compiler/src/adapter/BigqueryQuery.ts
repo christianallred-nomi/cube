2779b7a9660c3b7e0483e4c5f58427ba8888142a
Konstantin Burkalev <KSDaemon@gmail.com>
2024-08-29 19:19:13 +0300

    fix linting

--- packages/cubejs-schema-compiler/src/adapter/BigqueryQuery.ts
+++ packages/cubejs-schema-compiler/src/adapter/BigqueryQuery.ts
@@ -69,7 +69,7 @@ export class BigqueryQuery extends BaseQuery {
    */
   public dateBin(interval: string, source: string, origin: string): string {
     const intervalFormatted = this.formatInterval(interval);
-    const beginOfTime = this.dateTimeCast(`'1970-01-01T00:00:00'`);
+    const beginOfTime = this.dateTimeCast('\'1970-01-01T00:00:00\'');
 
     return `${this.dateTimeCast(`'${origin}'`)} + INTERVAL ${intervalFormatted} *
       CAST(FLOOR(
